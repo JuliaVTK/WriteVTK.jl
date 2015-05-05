96de03b454e4f62629e13313bf0268e87b785ddb
Juan Ignacio Polanco <jipolanc@gmail.com>
2015-05-05 20:02:17 -0300

    Update multiblock test, adding unstructured block

--- test/multiblock.jl
+++ test/multiblock.jl
@@ -4,6 +4,9 @@ using WriteVTK
 typealias FloatType Float32
 const vtm_filename_noext = "multiblock"
 
+# Like sub2ind, but specifically for 3D arrays.
+mysub2ind(dims, i, j, k) = i + dims[1]*(j-1) + dims[1]*dims[2]*(k-1)
+
 function first_block_data()
     const Ni, Nj, Nk = 10, 15, 20
 
@@ -44,6 +47,51 @@ function second_block_data()
     return x, y, z, q
 end
 
+function third_block_data()
+    # This is basically the same as in the unstructured example.
+    const Ni, Nj, Nk = 40, 50, 20
+    const dims = [Ni, Nj, Nk]
+
+    # Create points and point data.
+    pts = Array(FloatType, 3, Ni, Nj, Nk)
+    pdata = Array(FloatType, Ni, Nj, Nk)
+
+    for k = 1:Nk, j = 1:Nj, i = 1:Ni
+        r = 1 + (i - 1)/(Ni - 1)
+        th = 3*pi/4 * (j - 1)/(Nj - 1)
+        pts[1, i, j, k] = r * cos(th)
+        pts[2, i, j, k] = r * sin(th)
+        pts[3, i, j, k] = -2 + (k - 1)/(Nk - 1)
+        pdata[i, j, k] = i*i + k*sqrt(j)
+    end
+
+    # Create cells (all hexahedrons in this case) and cell data.
+    const celltype = VTKCellType.VTK_HEXAHEDRON
+    cells = MeshCell[]
+    cdata = FloatType[]
+
+    for k = 2:Nk, j = 2:Nj, i = 2:Ni
+        # Define connectivity of cell.
+        inds = Array(Int32, 8)
+        inds[1] = mysub2ind(dims, i-1, j-1, k-1)
+        inds[2] = mysub2ind(dims, i  , j-1, k-1)
+        inds[3] = mysub2ind(dims, i  , j  , k-1)
+        inds[4] = mysub2ind(dims, i-1, j  , k-1)
+        inds[5] = mysub2ind(dims, i-1, j-1, k  )
+        inds[6] = mysub2ind(dims, i  , j-1, k  )
+        inds[7] = mysub2ind(dims, i  , j  , k  )
+        inds[8] = mysub2ind(dims, i-1, j  , k  )
+
+        # Define cell.
+        c = MeshCell(celltype, inds)
+
+        push!(cells, c)
+        push!(cdata, i*j*k)
+    end
+
+    return pts, cells, pdata, cdata
+end
+
 function main()
     # Initialise multiblock file.
     vtm = vtk_multiblock(vtm_filename_noext)
@@ -57,6 +105,12 @@ function main()
     x, y, z, q = second_block_data()
     vtk = vtk_grid(vtm, x, y, z)
     vtk_point_data(vtk, q, "q_values")
+
+    # Create third block.
+    points, cells, q, c = third_block_data()
+    vtk = vtk_grid(vtm, points, cells)
+    vtk_point_data(vtk, q, "q_values")
+    vtk_cell_data(vtk, c, "c_values")
 
     # Saved multiblock file and included block files.
     outfiles = vtk_save(vtm)
