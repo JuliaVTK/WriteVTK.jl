a0cad62814bf5a9ed809fa7c8da3cedb63b53f99
Juan Ignacio Polanco <jipolanc@gmail.com>
2015-05-30 19:23:26 -0300

    Delete old documentation in code

--- src/WriteVTK.jl
+++ src/WriteVTK.jl
@@ -58,45 +58,4 @@ include("gridtypes/rectilinear.jl")
 # Common functions.
 include("gridtypes/common.jl")
 
-# TODO move this documentation!!
-#==========================================================================#
-# Creates a new grid file with coordinates x, y, z.
-#
-# The saved file has the name given by filename_noext, and its extension
-# depends on the type of grid, which is determined by the shape of the
-# arrays x, y, z.
-#
-# Accepted grid types:
-#   * Rectilinear grid      x, y, z are 1D arrays with possibly different
-#                           lengths.
-#
-#   * Structured grid       x, y, z are 3D arrays with the same dimensions.
-#                           TODO: also allow a single "4D" array, just like
-#                           for unstructured grids, which would give a
-#                           better performance.
-#
-#   * Unstructured grid     x is an array with all the points, with
-#                           dimensions [3, num_points].
-#                           cells is a MeshCell array with all the cells.
-#
-#                           NOTE: for convenience, there's also a separate
-#                           vtk_grid function specifically made for
-#                           unstructured grids.
-#
-# Optional parameters:
-# * If compress is true, written data is first compressed using Zlib.
-#   Set to false if you don't care about file size, or if speed is really
-#   important.
-#
-# * If append is true, data is appended at the end of the XML file as raw
-#   binary data.
-#   Otherwise, data is written inline, base-64 encoded. This is usually
-#   slower than writing raw binary data, and also results in larger files.
-#   Try disabling this if there are issues (for example with portability?),
-#   or if it's really important to follow the XML specifications.
-#
-#   Note that all combinations of compress and append are supported.
-#
-#==========================================================================#
-
 end     # module WriteVTK
