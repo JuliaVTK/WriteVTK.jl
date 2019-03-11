594644ae4be311f3885e84793758b89519f32569
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-03-11 10:50:37 -0300

    Allow multiple arrays to be saved at once in vtk_write_array

--- src/gridtypes/array.jl
+++ src/gridtypes/array.jl
@@ -1,4 +1,7 @@
 """
+    vtk_write_array(filename, arrays, labels)
+    vtk_write_array(filename, array, label="array")
+
 Write a Julia array to a VTK image data file (.vti).
 
 Useful for general visualisation of arrays.
@@ -6,12 +9,23 @@ The input can be a 2D or 3D array.
 
 """
 function vtk_write_array(filename::AbstractString,
-                         array::AbstractArray{T},
-                         label::AbstractString="array") where T <: Real
-    if !(2 <= ndims(array) <= 3)
+                         arrays::AbstractVector{A},
+                         labels::AbstractVector{String}) where {T<:Real,N,A<:AbstractArray{T,N}}
+    if !(2 <= N <= 3)
         throw(ArgumentError("Input should be a 2D or 3D array."))
     end
-    vtkfile = vtk_grid(filename, size(array)...)
-    vtk_point_data(vtkfile, array, label)
+    if length(arrays) != length(labels)
+      throw(ArgumentError("Number of arrays and labels must be equal."))
+    end
+    vtkfile = vtk_grid(filename, size(arrays[1])...)
+    for (array, label) in Iterators.zip(arrays, labels)
+      vtk_point_data(vtkfile, array, label)
+    end
     vtk_save(vtkfile)
+end
+
+function vtk_write_array(filename::AbstractString,
+                         array::AbstractArray{T},
+                         label::AbstractString="array") where T <: Real
+    vtk_write_array(filename, [array], [label])
 end
