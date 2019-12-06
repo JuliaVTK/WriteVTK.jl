a6ba300a65cafafe97ecc54c8ec2f25cd438225e
Juan Ignacio Polanco <jipolanc@gmail.com>
2019-12-06 21:07:04 +0100

    Define VTK cell types using macro

--- src/VTKCellTypes.jl
+++ src/VTKCellTypes.jl
@@ -15,6 +15,8 @@ struct VTKCellType
     nodes::Int
 end
 
+const VTK_CELL_TYPES = Dict{UInt8, VTKCellType}()
+
 # Construction of VTKCellType objects from the integer vtk id
 function VTKCellType(vtkid::Integer)
     id = UInt8(vtkid)
@@ -24,133 +26,76 @@ function VTKCellType(vtkid::Integer)
     VTK_CELL_TYPES[id]
 end
 
-# Linear cells
-const VTK_EMPTY_CELL       = VTKCellType("VTK_EMPTY_CELL",      UInt8(0),  0)
-const VTK_VERTEX           = VTKCellType("VTK_VERTEX",          UInt8(1),  1)
-const VTK_POLY_VERTEX      = VTKCellType("VTK_POLY_VERTEX",     UInt8(2), -1)
-const VTK_LINE             = VTKCellType("VTK_LINE",            UInt8(3),  2)
-const VTK_POLY_LINE        = VTKCellType("VTK_POLY_LINE",       UInt8(4), -1)
-const VTK_TRIANGLE         = VTKCellType("VTK_TRIANGLE",        UInt8(5),  3)
-const VTK_TRIANGLE_STRIP   = VTKCellType("VTK_TRIANGLE_STRIP",  UInt8(6), -1)
-const VTK_POLYGON          = VTKCellType("VTK_POLYGON",         UInt8(7), -1)
-const VTK_PIXEL            = VTKCellType("VTK_PIXEL",           UInt8(8),  4)
-const VTK_QUAD             = VTKCellType("VTK_QUAD",            UInt8(9),  4)
-const VTK_TETRA            = VTKCellType("VTK_TETRA",           UInt8(10), 4)
-const VTK_VOXEL            = VTKCellType("VTK_VOXEL",           UInt8(11), 8)
-const VTK_HEXAHEDRON       = VTKCellType("VTK_HEXAHEDRON",      UInt8(12), 8)
-const VTK_WEDGE            = VTKCellType("VTK_WEDGE",           UInt8(13), 6)
-const VTK_PYRAMID          = VTKCellType("VTK_PYRAMID",         UInt8(14), 5)
-const VTK_PENTAGONAL_PRISM = VTKCellType("VTK_PENTAGONAL_PRISM",UInt8(15),10)
-const VTK_HEXAGONAL_PRISM  = VTKCellType("VTK_HEXAGONAL_PRISM", UInt8(16),12)
-
-# Quadratic, isoparametric cells
-const VTK_QUADRATIC_EDGE                   = VTKCellType("VTK_QUADRATIC_EDGE",                  UInt8(21), 3)
-const VTK_QUADRATIC_TRIANGLE               = VTKCellType("VTK_QUADRATIC_TRIANGLE",              UInt8(22), 6)
-const VTK_QUADRATIC_QUAD                   = VTKCellType("VTK_QUADRATIC_QUAD",                  UInt8(23), 8)
-const VTK_QUADRATIC_POLYGON                = VTKCellType("VTK_QUADRATIC_POLYGON",               UInt8(36),-1)
-const VTK_QUADRATIC_TETRA                  = VTKCellType("VTK_QUADRATIC_TETRA",                 UInt8(24),10)
-const VTK_QUADRATIC_HEXAHEDRON             = VTKCellType("VTK_QUADRATIC_HEXAHEDRON",            UInt8(25),20)
-const VTK_QUADRATIC_WEDGE                  = VTKCellType("VTK_QUADRATIC_WEDGE",                 UInt8(26),15)
-const VTK_QUADRATIC_PYRAMID                = VTKCellType("VTK_QUADRATIC_PYRAMID",               UInt8(27),13)
-const VTK_BIQUADRATIC_QUAD                 = VTKCellType("VTK_BIQUADRATIC_QUAD",                UInt8(28), 9)
-const VTK_TRIQUADRATIC_HEXAHEDRON          = VTKCellType("VTK_TRIQUADRATIC_HEXAHEDRON",         UInt8(29),27)
-const VTK_QUADRATIC_LINEAR_QUAD            = VTKCellType("VTK_QUADRATIC_LINEAR_QUAD",           UInt8(30), 6)
-const VTK_QUADRATIC_LINEAR_WEDGE           = VTKCellType("VTK_QUADRATIC_LINEAR_WEDGE",          UInt8(31),12)
-const VTK_BIQUADRATIC_QUADRATIC_WEDGE      = VTKCellType("VTK_BIQUADRATIC_QUADRATIC_WEDGE",     UInt8(32),18)
-const VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON = VTKCellType("VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON",UInt8(33),24)
-const VTK_BIQUADRATIC_TRIANGLE             = VTKCellType("VTK_BIQUADRATIC_TRIANGLE",            UInt8(34), 7)
-
-# Cubic, isoparametric cell
-const VTK_CUBIC_LINE                       = VTKCellType("VTK_CUBIC_LINE",UInt8(35),4)
-
-# Special class of cells formed by convex group of points
-const VTK_CONVEX_POINT_SET = VTKCellType("VTK_CONVEX_POINT_SET",UInt8(41),-1)
-
-# Polyhedron cell (consisting of polygonal faces)
-const VTK_POLYHEDRON = VTKCellType("VTK_POLYHEDRON",UInt8(42),-1)
-
-# Higher order cells in parametric form
-const VTK_PARAMETRIC_CURVE        = VTKCellType("VTK_PARAMETRIC_CURVE",       UInt8(51),-1)
-const VTK_PARAMETRIC_SURFACE      = VTKCellType("VTK_PARAMETRIC_SURFACE",     UInt8(52),-1)
-const VTK_PARAMETRIC_TRI_SURFACE  = VTKCellType("VTK_PARAMETRIC_TRI_SURFACE", UInt8(53),-1)
-const VTK_PARAMETRIC_QUAD_SURFACE = VTKCellType("VTK_PARAMETRIC_QUAD_SURFACE",UInt8(54),-1)
-const VTK_PARAMETRIC_TETRA_REGION = VTKCellType("VTK_PARAMETRIC_TETRA_REGION",UInt8(55),-1)
-const VTK_PARAMETRIC_HEX_REGION   = VTKCellType("VTK_PARAMETRIC_HEX_REGION",  UInt8(56),-1)
-
-# Higher order cells
-const VTK_HIGHER_ORDER_EDGE        = VTKCellType("VTK_HIGHER_ORDER_EDGE",       UInt8(60),-1)
-const VTK_HIGHER_ORDER_TRIANGLE    = VTKCellType("VTK_HIGHER_ORDER_TRIANGLE",   UInt8(61),-1)
-const VTK_HIGHER_ORDER_QUAD        = VTKCellType("VTK_HIGHER_ORDER_QUAD",       UInt8(62),-1)
-const VTK_HIGHER_ORDER_POLYGON     = VTKCellType("VTK_HIGHER_ORDER_POLYGON",    UInt8(63),-1)
-const VTK_HIGHER_ORDER_TETRAHEDRON = VTKCellType("VTK_HIGHER_ORDER_TETRAHEDRON",UInt8(64),-1)
-const VTK_HIGHER_ORDER_WEDGE       = VTKCellType("VTK_HIGHER_ORDER_WEDGE",      UInt8(65),-1)
-const VTK_HIGHER_ORDER_PYRAMID     = VTKCellType("VTK_HIGHER_ORDER_PYRAMID",    UInt8(66),-1)
-const VTK_HIGHER_ORDER_HEXAHEDRON  = VTKCellType("VTK_HIGHER_ORDER_HEXAHEDRON", UInt8(67),-1)
-
-# Dictionary with all pre-defined VTKCellType objects
-
-function _init_VTK_CELL_TYPES()
-    # A better way to get a vector of predefined VTK cell types?
-    all_types = [
-      VTK_EMPTY_CELL,
-      VTK_VERTEX,
-      VTK_POLY_VERTEX,
-      VTK_LINE,
-      VTK_POLY_LINE,
-      VTK_TRIANGLE,
-      VTK_TRIANGLE_STRIP,
-      VTK_POLYGON,
-      VTK_PIXEL,
-      VTK_QUAD,
-      VTK_TETRA,
-      VTK_VOXEL,
-      VTK_HEXAHEDRON,
-      VTK_WEDGE,
-      VTK_PYRAMID,
-      VTK_PENTAGONAL_PRISM,
-      VTK_HEXAGONAL_PRISM,
-      VTK_QUADRATIC_EDGE,
-      VTK_QUADRATIC_TRIANGLE,
-      VTK_QUADRATIC_QUAD,
-      VTK_QUADRATIC_POLYGON,
-      VTK_QUADRATIC_TETRA,
-      VTK_QUADRATIC_HEXAHEDRON,
-      VTK_QUADRATIC_WEDGE,
-      VTK_QUADRATIC_PYRAMID,
-      VTK_BIQUADRATIC_QUAD,
-      VTK_TRIQUADRATIC_HEXAHEDRON,
-      VTK_QUADRATIC_LINEAR_QUAD,
-      VTK_QUADRATIC_LINEAR_WEDGE,
-      VTK_BIQUADRATIC_QUADRATIC_WEDGE,
-      VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON,
-      VTK_BIQUADRATIC_TRIANGLE,
-      VTK_CUBIC_LINE,
-      VTK_CONVEX_POINT_SET,
-      VTK_POLYHEDRON,
-      VTK_PARAMETRIC_CURVE,
-      VTK_PARAMETRIC_SURFACE,
-      VTK_PARAMETRIC_TRI_SURFACE,
-      VTK_PARAMETRIC_QUAD_SURFACE,
-      VTK_PARAMETRIC_TETRA_REGION,
-      VTK_PARAMETRIC_HEX_REGION,
-      VTK_HIGHER_ORDER_EDGE,
-      VTK_HIGHER_ORDER_TRIANGLE,
-      VTK_HIGHER_ORDER_QUAD,
-      VTK_HIGHER_ORDER_POLYGON,
-      VTK_HIGHER_ORDER_TETRAHEDRON,
-      VTK_HIGHER_ORDER_WEDGE,
-      VTK_HIGHER_ORDER_PYRAMID,
-      VTK_HIGHER_ORDER_HEXAHEDRON]
-
-    dict = Dict{UInt8,VTKCellType}()
-
-    for ctyp in all_types
-        dict[ctyp.vtk_id] = ctyp
+# Define a VTK cell type
+macro add(ctype, id, nodes)
+    name = string(ctype)
+    quote
+        const $(esc(ctype)) = VTKCellType($name, $id, $nodes)
+        VTK_CELL_TYPES[$id] = $(esc(ctype))
     end
-
-    dict
 end
 
-const VTK_CELL_TYPES = _init_VTK_CELL_TYPES()
+# Linear cells
+@add VTK_EMPTY_CELL 0 0
+@add VTK_VERTEX 1  1
+@add VTK_POLY_VERTEX 2 -1
+@add VTK_LINE 3  2
+@add VTK_POLY_LINE 4 -1
+@add VTK_TRIANGLE 5  3
+@add VTK_TRIANGLE_STRIP 6 -1
+@add VTK_POLYGON 7 -1
+@add VTK_PIXEL 8  4
+@add VTK_QUAD 9  4
+@add VTK_TETRA 10 4
+@add VTK_VOXEL 11 8
+@add VTK_HEXAHEDRON 12 8
+@add VTK_WEDGE 13 6
+@add VTK_PYRAMID 14 5
+@add VTK_PENTAGONAL_PRISM 15 10
+@add VTK_HEXAGONAL_PRISM 16 12
+
+# Quadratic, isoparametric cells
+@add VTK_QUADRATIC_EDGE 21 3
+@add VTK_QUADRATIC_TRIANGLE 22 6
+@add VTK_QUADRATIC_QUAD 23 8
+@add VTK_QUADRATIC_POLYGON 36 -1
+@add VTK_QUADRATIC_TETRA 24 10
+@add VTK_QUADRATIC_HEXAHEDRON 25 20
+@add VTK_QUADRATIC_WEDGE 26 15
+@add VTK_QUADRATIC_PYRAMID 27 13
+@add VTK_BIQUADRATIC_QUAD 28 9
+@add VTK_TRIQUADRATIC_HEXAHEDRON 29 27
+@add VTK_QUADRATIC_LINEAR_QUAD 30 6
+@add VTK_QUADRATIC_LINEAR_WEDGE 31 12
+@add VTK_BIQUADRATIC_QUADRATIC_WEDGE 32 18
+@add VTK_BIQUADRATIC_QUADRATIC_HEXAHEDRON 33 24
+@add VTK_BIQUADRATIC_TRIANGLE 34 7
+
+# Cubic, isoparametric cell
+@add VTK_CUBIC_LINE 35 4
+
+# Special class of cells formed by convex group of points
+@add VTK_CONVEX_POINT_SET 41 -1
+
+# Polyhedron cell (consisting of polygonal faces)
+@add VTK_POLYHEDRON 42 -1
+
+# Higher order cells in parametric form
+@add VTK_PARAMETRIC_CURVE 51 -1
+@add VTK_PARAMETRIC_SURFACE 52 -1
+@add VTK_PARAMETRIC_TRI_SURFACE 53 -1
+@add VTK_PARAMETRIC_QUAD_SURFACE 54 -1
+@add VTK_PARAMETRIC_TETRA_REGION 55 -1
+@add VTK_PARAMETRIC_HEX_REGION 56 -1
+
+# Higher order cells
+@add VTK_HIGHER_ORDER_EDGE 60 -1
+@add VTK_HIGHER_ORDER_TRIANGLE 61 -1
+@add VTK_HIGHER_ORDER_QUAD 62 -1
+@add VTK_HIGHER_ORDER_POLYGON 63 -1
+@add VTK_HIGHER_ORDER_TETRAHEDRON 64 -1
+@add VTK_HIGHER_ORDER_WEDGE 65 -1
+@add VTK_HIGHER_ORDER_PYRAMID 66 -1
+@add VTK_HIGHER_ORDER_HEXAHEDRON 67 -1
 
 end # module
