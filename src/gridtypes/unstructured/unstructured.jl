981a4bc0a299bcd38649ac2b7d331021de3c5cf1
Juan Ignacio Polanco <jipolanc@gmail.com>
2021-03-29 11:55:37 +0200

    Update doctests for Julia 1.6

--- src/gridtypes/unstructured/unstructured.jl
+++ src/gridtypes/unstructured/unstructured.jl
@@ -32,7 +32,7 @@ Define a triangular cell passing by points with indices `[3, 5, 42]`.
 
 ```jldoctest
 julia> cell = MeshCell(VTKCellTypes.VTK_TRIANGLE, [3, 5, 42])
-MeshCell{VTKCellType,Array{Int64,1}}(VTKCellType("VTK_TRIANGLE", 0x05, 3), [3, 5, 42])
+MeshCell{VTKCellType, Vector{Int64}}(VTKCellType("VTK_TRIANGLE", 0x05, 3), [3, 5, 42])
 ```
 """
 struct MeshCell{CellType, V <: Connectivity}
