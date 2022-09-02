62bed2a9bf4b601b6dc03e990b7321d0c93f6609
Juan Ignacio Polanco <jipolanc@gmail.com>
2022-09-02 15:16:04 +0200

    More tests

--- test/surface.jl
+++ test/surface.jl
@@ -1,11 +1,29 @@
 using WriteVTK
+using Test
 
 xs = (0:0.1:1).^2
 ys = 0:0.2:4
 
 zs = @. xs * sinpi(ys')
 
-@time vtk_surface("surface", xs, ys, zs; compress = false, append = false) do vtk
-    vtk["point_data"] = zs
-    vtk["cell_data"] = @views abs2.(zs[2:end, 2:end])
+files = String[]
+
+@test_throws DimensionMismatch vtk_surface("will_fail", xs, [0.3, 0.4], zs)
+
+let
+    @time output = vtk_surface("surface_basic", xs, ys, zs)
+    append!(files, output)
 end
+
+let
+    @time output = vtk_surface(
+            "surface", xs, ys, zs;
+            compress = false, append = false,
+        ) do vtk
+        vtk["point_data"] = zs
+        vtk["cell_data"] = @views abs2.(zs[2:end, 2:end])
+    end
+    append!(files, output)
+end
+
+files
